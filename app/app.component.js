4ca2d935f6b5fbeef1152468181b9ec46437bdc0
Daniel Douglas Machado <danielprogramic@gmail.com>
2017-01-25 17:36:45 -0200

    Create Product List

--- app/app.component.js
+++ app/app.component.js
@@ -5,9 +5,6 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 //import for use in core angular.
 var core_1 = require("@angular/core");
 // create metadata decor
@@ -21,8 +18,7 @@ AppComponent = __decorate([
     core_1.Component({
         selector: 'pm-app',
         template: "\n    <div><h1>{{pageTitle}}</h1>\n        <pm-products> </pm-products>\n    </div>\n    "
-    }),
-    __metadata("design:paramtypes", [])
+    })
 ], AppComponent);
 exports.AppComponent = AppComponent;
 //# sourceMappingURL=app.component.js.map
